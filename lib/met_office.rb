404e676d5fb1c901576e08da383a6b024baa1fe6
Maya Driver <mayadrivermd@gmail.com>
2014-08-26 14:12:46 +0100

    edited met office

--- lib/met_office.rb
+++ lib/met_office.rb
@@ -3,8 +3,6 @@ module MetOffice
 	def weather_report
 		@weather = ['sunny', 'windy'].sample
 		@weather
-		# weather = rand(10)
-		# weather > 5 ? @weather = :windy : @weather = :sunny
 	end
 
 end
