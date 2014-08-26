147c9428c74b32b3bde54835e2b718c28f549f3a
Maya Driver <mayadrivermd@gmail.com>
2014-08-26 22:04:46 +0100

    refactored airport spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,41 +6,33 @@ describe Airport do
 	let(:gatwick)  {Airport.new}
 	let(:plane)	   {Plane.new}
 
-	def sunny
-		allow(gatwick).to receive(:weather_report).and_return('sunny')
-	end
-
-	def windy
-		allow(gatwick).to receive(:weather_report).and_return('windy')
-	end
-
 	def fill_airport(gatwick)
 		(Airport::DEFAULT_CAPACITY).times {gatwick.arrivals(plane)}
 	end
 
 	context 'for arrivals and departures' do
 
+		before(:each) do
+			allow(gatwick).to receive(:weather_report).and_return('sunny')
+		end
+
 		it 'should allow planes to land in good weather' do
-			sunny
 			gatwick.arrivals(plane)
 			expect(gatwick).to have_planes 
 		end
 
 		it 'should know a plane is landed after landing' do
-			sunny
 			expect(plane).to receive(:land!).and_return(:landed)
 			gatwick.arrivals(plane)
 		end
 
 		it 'should allow planes to take off when it is sunny' do
-			sunny
 			gatwick.arrivals(plane)
 			gatwick.departures(plane)
 			expect(gatwick).not_to have_planes
 		end
 
 		it 'should know a plane is flying after take off' do
-			sunny
 			expect(plane).to receive(:fly!).and_return(:flying)
 			gatwick.departures(plane)
 
@@ -50,20 +42,21 @@ describe Airport do
 
 	context 'traffic control' do
 
+		before(:each) do
+			allow(gatwick).to receive(:weather_report).and_return('sunny')
+		end
+
 		it 'should know how many planes it has' do
-			sunny
 			gatwick.arrivals(plane)
 			expect(gatwick.planes_count).to eq(1)
 		end
 
 		it 'should know when it is full' do
-			sunny
 			fill_airport(gatwick)
 			expect(gatwick).to be_full
 		end
 
 		it 'should not allow planes to land when it is full' do
-			sunny
 			fill_airport(gatwick)
 			expect(lambda { gatwick.arrivals(plane) }).to raise_error
 		end
@@ -72,13 +65,15 @@ describe Airport do
 
 	context 'regarding the weather' do
 
+		before(:each) do
+			allow(gatwick).to receive(:weather_report).and_return('windy')
+		end
+
 		it 'should not allow planes to take off in strong winds' do
-			windy
 			expect(lambda{gatwick.departures(plane)}).to raise_error
 		end
 
 		it 'should not allow planes to land in strong winds' do
-			windy
 			expect(lambda{gatwick.arrivals(plane)}).to raise_error
 		end
 
